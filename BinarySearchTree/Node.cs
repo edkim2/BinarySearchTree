0aaf6d3a8d56ae33b5c7bc2b63d0a096d73bfd69
edkim2 <ekim10203@gmail.com>
2018-03-08 09:14:52 -0600

    Initial commit. Created a Tree and Node class.

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        // member variables
+        public int value;
+        public Node left;
+        public Node right;
+        // constructor
+        public Node()
+        {
+
+        }
+        // member methods
+    }
+}
